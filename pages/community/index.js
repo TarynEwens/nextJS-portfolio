2479f86bd403ac110a7bd8b0f72753af7da9dd3d
tarynewens <taryn@taryn.codes>
2021-10-25 18:35:09 +1100

    [Community] update GA description

--- pages/community/index.js
+++ pages/community/index.js
@@ -35,17 +35,16 @@ export default function Community() {
           </p>
 
           <p>
-            I have previously presented to software engineering classes at{" "}
+            I also present to software engineering classes at{" "}
             <a
               className={utilStyles.linkFeature}
               href="https://generalassemb.ly/"
             >
               General Assembly
             </a>{" "}
-            Sydney (and Melbourne once or twice!). I've introduced them to CSS,
-            the amazing things that can be created with it (think fun
-            interactions and art!), and resources for them to continue learning
-            more!
+            Sydney (and Melbourne once or twice!). I introduce them to CSS, the
+            amazing things that can be created with it (think fun interactions
+            and art!), and resources for them to continue learning more!
           </p>
           <p></p>
         </section>
